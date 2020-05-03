b4db551e50465d3f493beec2384d62b0057f9424
unknown <becherers7@gmail.com>
2020-05-03 14:34:44 -0500

    Commiting changes.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+// import logo from '../img/logo.svg'
+import logo from "../img/LOGO-A.svg"
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -42,7 +43,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              Project AERO
             </Link>
             {/* Hamburger menu */}
             <div
